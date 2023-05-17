8bcc3f1c0681edba90abde5d5746c45aac132fcc
endenwer <endenwer@gmail.com>
2023-05-17 16:13:01 +0300

    Add user table

---
+++ db/migrate/20230517131032_create_user.rb
@@ -0,0 +1,11 @@
+class CreateUser < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.integer :telegram_id, null: false
+      t.string :first_name, null: false
+      t.string :username
+      t.string :last_name
+      t.timestamps
+    end
+  end
+end
