85ce31badedf230a05f21225c72ea0e6567802ec
Ilya Stankevich <soundbroo@gmail.com>
2023-06-22 18:02:16 +0300

    add specs

--- app/services/profile_service.rb
+++ app/services/profile_service.rb
@@ -2,13 +2,15 @@ class ProfileService
   include RespondService
 
   def call(user)
-    profile_info = I18n.t('profile_service.profile_info',
-                          telegram_id: user.telegram_id,
-                          username: user.username,
-                          first_name: user.first_name,
-                          last_name: user.last_name,
-                          games_finished: user.games.finished.size,
-                          games_canceled: user.games.canceled.size)
+    profile_info = I18n.t(
+      'profile_service.profile_info',
+      telegram_id: user.telegram_id,
+      username: user.username,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      games_finished: user.games.finished.size,
+      games_canceled: user.games.canceled.size
+    )
     success(profile_info)
   end
 end
